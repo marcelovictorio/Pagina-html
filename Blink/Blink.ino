513532ca74fd403e8d37d8c98de920c97b128046
Marcelo <marcelo_victorio@hotmail.com>
2024-07-08 15:36:49 -0400

    Novo teste

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -1,5 +1,5 @@
 /*
-  Blink
+  Blink <Alterado por Marcelo>
 */
 
 // the setup function runs once when you press reset or power the board
@@ -15,10 +15,10 @@ void setup() {
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
   Serial.println("ALERTA: LED LIGADO"); // print alert message to the serial monitor
-  delay(1000);                       // wait for a second
+  delay(100);                       // wait for a second
 
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
   Serial.println("ALERTA: LED DESLIGADO"); // print alert message to the serial monitor
-  delay(1000);                       // wait for a second
+  delay(100);                       // wait for a second
 }
   
